d66b8a02361cd61a906c563dc8d4c8a5ebd0810a
Pedro <pedro_h_bueno@estudante.sesisenai.org.br>
2025-04-19 23:40:49 -0300

    UPT - adição de JS até onde consegui

---
+++ script.js
@@ -0,0 +1,54 @@
+const form = document.getElementById('form');
+const Jnome = document.getElementById('Jname');
+const Pnome = document.getElementById('Pname');
+const nascimento = document.getElementById('nascimento');
+const idade = document.getElementById('idade');
+const altura = document.getElementById('altura');
+const nivel = document.getElementById('nivel');
+const peso = document.getElementById('peso');
+const deslocamento = document.getElementById('deslocamento');
+const Pvida = document.getElementById('Pvida');
+const Dvida = document.getElementById('Dvida');
+const raça = document.getElementById('raça');
+const Antecedente = document.getElementById('Antecedente');
+const alinhamento = document.getElementById('alinhamento');
+const conjuração = document.getElementById('conjuração');
+const CDmagias = document.getElementById('CDmagias');
+const MODmagia = document.getElementById('MODmagia');
+const Sforça = document.getElementById('Sforça');
+const Sdestreza = document.getElementById('Sdestreza');
+const Sconstituição = document.getElementById('Sconstituição');
+const SSabedoria = document.getElementById('SSabedoria');
+const SInteligência = document.getElementById('SInteligência');
+const SCarisma = document.getElementById('SCarisma');
+const armas1 = document.getElementById('armas1');
+const armas2 = document.getElementById('armas2');
+const armas3 = document.getElementById('armas3');
+const armaduras = document.getElementById('armaduras');
+const escudo = document.getElementsByName('escudo');
+const classe = document.getElementsByName('classe');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+form.addEventListener("submit",(event) =>{
+    event.preventDefault();
+    alert("deu certo viado")
+} )
+
